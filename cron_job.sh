8cd7a15ebf2599151117e39d37d96b4155e55e7e
paulproteus <paulproteus@committer.creativecommons.org>
2009-04-10 21:02:07 +0000

    point events map thingamabob to opened.cc instead!

--- cron_job.sh
+++ cron_job.sh
@@ -5,7 +5,7 @@ YEAR=$(date +%Y)
 MONTH=$(date +%m)
 DAY=$(date +%d)
 # DONE: CSV_URL includes today's date!
-CSV_URL="http://wiki.creativecommons.org/Special:Ask/-5B-5Bdate::-3E$YEAR-2D$MONTH-2D$DAY-5D-5D-20-0A-20-20-5B-5BCategory::Event-5D-5D/%3FEventType/%3FDate/%3FLocation/%3FAttendance/%3FMainurl/%3FEventCategory/sort%3DDate/order%3DASC/format%3Dcsv/limit%3D1000"
+CSV_URL="http://opened.creativecommons.org/Special:Ask/-5B-5Bdate::-3E$YEAR-2D$MONTH-2D$DAY-5D-5D-20-0A-20-20-5B-5BCategory::Event-5D-5D/%3FEventType/%3FDate/%3FLocation/%3FAttendance/%3FMainurl/%3FEventCategory/sort%3DDate/order%3DASC/format%3Dcsv/limit%3D1000"
 FINAL_TEXTFILE_PATH="/home/paulproteus/events-map/textfile.txt"
 
 ### Code:
